29f3a45ed4ff6a0a6c195d58c66de292dbcff2ae
Haiqa461 <2022ee255@student.uet.edu.pk>
2024-04-16 23:41:06 +0500

    one more done

--- lab_8.cpp
+++ lab_8.cpp
@@ -2,78 +2,78 @@
 #include <fstream>
 using namespace std;
 
-// Define Node structure
+// Node structure
 struct Node {
-    int value;
-    Node *next;
+    int data;
+    Node* next;
 };
 
 // Global variable for the start of the linked list
-Node *head = nullptr;
+Node* start = nullptr;
 
 // Function prototypes
 void create(int data);
-void insert(int data);
+void add(int data);
 void remove(int data);
-int listLength();
-void printList();
-void saveListToFile();
+int length();
+void traverse(bool increasing);
+void storeListToFile();
 
 // Function to create the very first node
 void create(int data) {
-    Node *newNode = new Node;
-    newNode->value = data;
+    Node* newNode = new Node;
+    newNode->data = data;
     newNode->next = nullptr;
-    head = newNode;
+    start = newNode;
 }
 
 // Function to insert numbers in sorted order
-void insert(int data) {
-    if (head == nullptr || data < head->value) {
-        Node *newNode = new Node;
-        newNode->value = data;
-        newNode->next = head;
-        head = newNode;
+void add(int data) {
+    if (start == nullptr || data < start->data) {
+        Node* newNode = new Node;
+        newNode->data = data;
+        newNode->next = start;
+        start = newNode;
         return;
     }
 
-    Node *current = head;
-    Node *prev = nullptr;
-    while (current != nullptr && current->value < data) {
+    Node* current = start;
+    Node* prev = nullptr;
+    while (current != nullptr && current->data < data) {
         prev = current;
         current = current->next;
     }
-    Node *newNode = new Node;
-    newNode->value = data;
+    Node* newNode = new Node;
+    newNode->data = data;
     newNode->next = current;
     prev->next = newNode;
 }
 
 // Function to delete a node
 void remove(int data) {
-    Node *current = head;
-    Node *prev = nullptr;
-    while (current != nullptr && current->value != data) {
+    Node* current = start;
+    Node* prev = nullptr;
+    while (current != nullptr && current->data != data) {
         prev = current;
         current = current->next;
     }
     if (current == nullptr) {
-        cout << "Node not found" << endl;
+        cout << "Not found" << endl;
         return;
     }
     if (prev == nullptr) {
-        head = current->next;
+        start = current->next;
     } else {
         prev->next = current->next;
     }
     delete current;
-    cout << "Deleted node with value " << data << endl;
+    cout << "Deleted " << data << endl;
 }
 
 // Function to find the length of the linked list
-int listLength() {
+int length() {
     int count = 0;
-    Node *current = head;
+    Node* current = start;
     while (current != nullptr) {
         count++;
         current = current->next;
@@ -82,41 +82,53 @@ int listLength() {
 }
 
 // Function to traverse and print elements of the linked list
-void printList() {
-    Node *current = head;
-    while (current != nullptr) {
-        cout << current->value << " ";
-        current = current->next;
+void traverse(bool increasing) {
+    Node* current = start;
+    if (increasing) {
+        while (current != nullptr) {
+            cout << current->data << " ";
+            current = current->next;
+        }
+    } else {
+        Node* prev = nullptr;
+        while (start != nullptr) {
+            Node* next = start->next;
+            start->next = prev;
+            prev = start;
+            start = next;
+        }
+        start = prev;
+        traverse(true);
     }
     cout << endl;
 }
 
 // Function to store the linked list to a file
-void saveListToFile() {
+void storeListToFile() {
     ofstream outputFile("output.txt");
     if (outputFile.is_open()) {
-        Node *current = head;
+        Node* current = start;
         while (current != nullptr) {
-            outputFile << current->value << " ";
+            outputFile << current->data << " ";
             current = current->next;
         }
         outputFile.close();
-        cout << "Linked list saved to output.txt" << endl;
+        cout << "Linked list stored in output.txt" << endl;
     } else {
         cout << "Unable to open output.txt for writing" << endl;
     }
 }
 
 int main() {
-    // Create and populate the linked list from an external file
+    // Create and populate the linked list from external file
     ifstream inputFile("numbers.txt");
     if (inputFile.is_open()) {
         int num;
         while (inputFile >> num) {
-            if (head == nullptr) {
+            if (start == nullptr) {
                 create(num);
             } else {
-                insert(num);
+                add(num);
             }
         }
         inputFile.close();
@@ -131,9 +143,10 @@ int main() {
         cout << "Choose an action:" << endl;
         cout << "1. Add a node" << endl;
         cout << "2. Delete a node" << endl;
-        cout << "3. Enumerate the elements of the list" << endl;
-        cout << "4. Length of the list" << endl;
-        cout << "5. End and store the current linked list" << endl;
+        cout << "3. Enumerate the elements of the list in increasing order" << endl;
+        cout << "4. Enumerate the elements of the list in decreasing order" << endl;
+        cout << "5. Length of the list" << endl;
+        cout << "6. End and store the current linked list" << endl;
         cout << "Enter your choice: ";
         cin >> choice;
 
@@ -142,7 +155,7 @@ int main() {
                 int data;
                 cout << "Enter data to add: ";
                 cin >> data;
-                insert(data);
+                add(data);
                 break;
             }
             case 2: {
@@ -153,20 +166,24 @@ int main() {
                 break;
             }
             case 3:
-                cout << "Elements of the list: ";
-                printList();
+                cout << "Elements of the list in increasing order: ";
+                traverse(true);
                 break;
             case 4:
-                cout << "Length of the list: " << listLength() << endl;
+                cout << "Elements of the list in decreasing order: ";
+                traverse(false);
                 break;
             case 5:
-                saveListToFile();
+                cout << "Length of the list: " << length() << endl;
+                break;
+            case 6:
+                storeListToFile();
                 break;
             default:
                 cout << "Invalid choice. Please try again." << endl;
         }
 
-    } while (choice != 5);
+    } while (choice != 6);
 
     return 0;
 }
